e77f0355f87500f5f889b874c02c464b28abe3a0
SQ Shabani <132075199+SQSh1@users.noreply.github.com>
2025-05-09 18:05:48 +0330

    Create PrivacyHooks.kt

---
+++ module/src/main/java/sq/powergram/hook/PrivacyHooks.kt
@@ -0,0 +1,68 @@
+package sq.powergram.hook
+
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+object PrivacyHooks {
+
+    fun apply(lpparam: XC_LoadPackage.LoadPackageParam) {
+        hideReadReceipts(lpparam)
+        hideTypingStatus(lpparam)
+        hideOnlineStatus(lpparam)
+        showDeletedMessages(lpparam)
+    }
+
+    private fun hideReadReceipts(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XposedHelpers.findAndHookMethod(
+            "org.telegram.messenger.MessagesController",
+            lpparam.classLoader,
+            "markDialogAsRead",
+            Long::class.javaPrimitiveType,
+            Boolean::class.javaPrimitiveType,
+            object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    param.result = null
+                }
+            }
+        )
+    }
+
+    private fun hideTypingStatus(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XposedHelpers.findAndHookMethod(
+            "org.telegram.messenger.SendMessagesHelper",
+            lpparam.classLoader,
+            "sendTyping",
+            Long::class.javaPrimitiveType,
+            object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    param.result = null
+                }
+            }
+        )
+    }
+
+    private fun hideOnlineStatus(lpparam: XC_LoadPackage.LoadPackageParam) {
+        // TODO: Hook واقعی برای مخفی کردن وضعیت آنلاین باید بررسی و توسعه داده شود
+    }
+
+    private fun showDeletedMessages(lpparam: XC_LoadPackage.LoadPackageParam) {
+        try {
+            XposedHelpers.findAndHookMethod(
+                "org.telegram.messenger.MessageObject",
+                lpparam.classLoader,
+                "isOut",
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        val result = param.result as Boolean
+                        if (!result) {
+                            XposedHelpers.setBooleanField(param.thisObject, "deleted", false)
+                        }
+                    }
+                }
+            )
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
+}
