31a4160e770d57b15d3f91a5b99999f8242977a6
SQ Shabani <132075199+SQSh1@users.noreply.github.com>
2025-05-09 20:03:21 +0330

    Update SettingsActivity.kt

--- module/src/main/java/sq/powergram/ui/SettingsActivity.kt
+++ module/src/main/java/sq/powergram/ui/SettingsActivity.kt
@@ -1,16 +1,11 @@
 package sq.powergram.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
 import sq.powergram.R
 
-class SettingsActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(android.R.id.content, SettingsFragment())
-            .commit()
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.prefs_privacy)
     }
 }
